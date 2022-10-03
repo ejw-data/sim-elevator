17194e9e72e5e55b36fe5d83f9d29ead4d07a8bf
Erin Wills <ejw.data@gmail.com>
2022-10-03 02:36:54 -0500

    finished test case for scheduling resources - works fine

--- scheduling_test_stores.py
+++ scheduling_test_stores.py
@@ -1,47 +1,43 @@
+# packages needed
 import simpy
 import random
 
-def user(env, trial, machine, line):
-  
+# function/generator that models the process
+def elevator_request(env, trial, machine, line):
+    # Request and checkout Resource
     with line.request() as request:
         yield request
     
+        # Checkout Store and access stored parameters
         m = yield machine.get()
         print(f"Trial {trial:<3} Time {env.now:<5} {m['name']} Starts")
+        
+        # Logic for the Resource and associated Store
         yield env.timeout(random.randint(1,15))
         print(f"Trial {trial:<3} Time {env.now:<5} {m['name']} Finished")
+        
+        # Checkin Store
         yield machine.put(m)
         
-        
+# create entities to be introduced to the process        
 def setup():
+    # create 10 elevator requests separated by 3 time units
     for i in range(10):
-        env.process(user(env, i, machine, production_line))
+        env.process(elevator_request(env, i, machine, production_line))
         yield env.timeout(3)
-    # yield line.request()
-    # m = yield machine.get()
-    # print(env.now, " >> ", m)
-    # yield machine.put(m)
-    # env.timeout(1)
 
-    # yield line.request()
-    # m = yield machine.get()
-    # print(env.now, " >> ", m)
-    # yield machine.put(m)
-    # env.timeout(1)
-
-    # yield line.request()
-    # m = yield machine.get()
-    # print(env.now, " >> ", m)
-    # yield machine.put(m)
-    # env.timeout(1)
-
-
+# create environment
 env = simpy.Environment()
+
+# assign number of resources and parameters
 production_line = simpy.Resource(env, 3)
 machine = simpy.FilterStore(env, 3)
 machine.put({'id': 0, 'name': 'Elevator 1'})
 machine.put({'id': 1, 'name': 'Elevator 2'})
 machine.put({'id': 2, 'name': 'Elevator 3'})
 
+# add initial function to env and chained to other functions
 env.process(setup())
+
+# run environment and therefore setup() function
 env.run(until=50)
