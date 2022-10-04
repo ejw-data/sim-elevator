1bbfd4c0cf26ed982109a45843b590b083fd7286
Erin Wills <ejw.data@gmail.com>
2022-10-04 01:56:08 -0500

    renamed files and moved test files to a folder

--- elevator.py
+++ elevator.py
@@ -1,33 +1,82 @@
+# Converstion of simpy model to Object Oriented
+# - aids in structuring the code
+
 import random
 import simpy
 
-
-RANDOM_SEED = 42
-NUMBER_ELEVATORS = 1
-NORMAL_SPEED_ELEVATOR = 3
-STOP_SPEED_ELEVATOR = 5
-
-# WASHTIME = 5      # Minutes it takes to clean a car
-REQUEST_INTERVAL = 7       # Create a car every ~7 minutes
-SIM_TIME = 120     # Simulation time in minutes
+class ModelConstants:
+    random_seed = 42
+    number_elevators = 9
+    normal_speed_elevator = 3
+    stop_speed_elevator = 5
+    sim_duration = 600
 
 # current_floor tracking doesn't work for multi-elevator system
-current_floor = 1
+# Global Variables
 total_trips = 0
 
-class ElevatorBank(object):
+class User:
+    def __init__(self, user_id, initial_floor, destination):
+        self.id = user_id
+        self.name = f'Button Press {user_id}'
+        self.initial_floor = initial_floor
+        self.destination = destination
 
-    def __init__(self, env, num_elevators, normal_speed, stop_speed, current_elevator_floor):
-        self.env = env
-        self.elevator = simpy.Resource(env, num_elevators)
-        self.normal_speed = normal_speed
-        self.stop_speed = stop_speed
-        self.current_floor = current_elevator_floor
+class ElevatorBank:
 
-    def call(self, name, floor):
-        global current_floor 
+    def __init__(self):
+        self.env = simpy.Environment()
+        self.elevator = simpy.Resource(self.env, ModelConstants.number_elevators)
+        self.elevator_info = simpy.FilterStore(self.env, ModelConstants.number_elevators)
+        self.user_counter = 0
+        self.registered_elevators = 0
+        self.total_floors = {}
+        self.elevator_trips = {}
+        self.elevator_floor = {}
+
+
+    def add_elevator(self):
+        self.registered_elevators += 1
+        elevator_name = f"Elevator_{self.registered_elevators}"
+        self.total_floors[elevator_name] = 0
+        self.elevator_trips[elevator_name] = 0
+        self.elevator_floor[elevator_name] = 1
+        self.elevator_info.put({
+            'id':self.registered_elevators, 
+            'name':elevator_name, 
+            'current_floor':1,
+            'trips':0
+            }
+        )
+
+    def remove_elevator(self):
+        pass
+        # future code for taking existing elevator out of service
+
+    def generate_user(self):
+        initial_floor = 1
+        destination = random.randint(2,19)
+
+        # while True:
+        self.user_counter += 1
+        new_user = User(self.user_counter, initial_floor, destination)
+        yield self.env.process(self.user_request(new_user))
+
+    def user_request(self, user):
+        print(f'{user.name} occurs at {self.env.now:.2f}.')
+        with self.elevator.request() as request:
+            yield request 
+
+            checkout_elevator = yield self.elevator_info.get()
+            print(f"Trial {user.id:<3} Time {self.env.now:<5} {checkout_elevator['name']} Starts")
+
+            print(f'{user.name}:  Elevator begins to move from Floor {user.initial_floor} to Floor {user.destination} at {self.env.now:.2f}.')
+            yield self.env.process(self.call(checkout_elevator, user.name, user.destination))
+
+    def call(self, elevator, name, floor):
         global total_trips
-
+        current_floor = elevator['current_floor']
+        
         if current_floor > floor:
             elevator_path = range(current_floor, floor, -1)
             slow_floor = floor + 1
@@ -39,71 +88,76 @@ class ElevatorBank(object):
 
         for elevator_floor in elevator_path:
             if elevator_floor != slow_floor:
-                yield self.env.timeout(self.normal_speed)
-                print(f"{name}:  Located at Floor {elevator_floor+direction} at {env.now:.2f}")
+                yield self.env.timeout(ModelConstants.normal_speed_elevator)
+                print(f"{name}:  Located at Floor {elevator_floor+direction} at {self.env.now:.2f}")
                 current_floor = elevator_floor + direction
+                self.total_floors[elevator['name']] += 1
+                elevator['current_floor'] = current_floor
+                self.elevator_floor[elevator['name']] = current_floor
             else:
-                yield self.env.timeout(self.stop_speed)
-                print(f"{name}:  Doors open on Floor {floor} at {env.now:.2f}")
+                yield self.env.timeout(ModelConstants.stop_speed_elevator)
+                print(f"{name}:  Doors open on Floor {floor} at {self.env.now:.2f}")
                 current_floor = elevator_floor + direction
-                
+                self.total_floors[elevator['name']] += 1
+                elevator['current_floor'] = current_floor
+                self.elevator_floor[elevator['name']] = current_floor
 
         yield self.env.timeout(2)
-        print(f'{name}: Doors close at {env.now:.2f}')
+        print(f'{name}: Doors close at {self.env.now:.2f}')
 
         #  select new floor destination
         #  initially make it go to floor one
         first_floor = 1
         for elevator_floor in range(current_floor-1, 0,-1):
             if elevator_floor != first_floor:
-                yield self.env.timeout(self.normal_speed)
-                print(f"{name}:  Located at Floor {elevator_floor} at {env.now:.2f}")
+                yield self.env.timeout(ModelConstants.normal_speed_elevator)
+                print(f"{name}:  Located at Floor {elevator_floor} at {self.env.now:.2f}")
                 current_floor = elevator_floor -1 
+                self.total_floors[elevator['name']] += 1
+                elevator['current_floor'] = current_floor
+                self.elevator_floor[elevator['name']] = current_floor
             else:
-                yield self.env.timeout(self.stop_speed)
-                print(f"{name}:  Doors open on Floor 1 at {env.now:.2f}")
+                yield self.env.timeout(ModelConstants.stop_speed_elevator)
+                print(f"{name}:  Doors open on Floor 1 at {self.env.now:.2f}")
                 total_trips += 1
+                current_floor = elevator_floor -1 
+                self.total_floors[elevator['name']] += 1
+                elevator['trips'] += 1
+                self.elevator_floor[elevator['name']] = current_floor
+                self.elevator_trips[elevator['name']] += 1
 
-def user(env, name, elevator_instance, call_floor):
-    print(f'{name} occurs at {env.now:.2f}.')
-    with elevator_instance.elevator.request() as request:
-        yield request   
-        print(f'{name}:  Elevator begins to move from Floor {current_floor} to Floor {call_floor} at {env.now:.2f}.')
-        yield env.process(elevator_instance.call(name, call_floor))
+        yield self.env.timeout(2)
+        print(f'{name}: Doors close at {self.env.now:.2f}')
 
+        yield self.elevator_info.put(elevator)
 
-def setup(env, num_elevators, normal_speed, stop_speed, current_elevator_floor):
+    def setup(self):
+        # create elevator lists
+        for _ in range(ModelConstants.number_elevators):
+            self.add_elevator()
 
-    building_elevator = ElevatorBank(env, num_elevators, normal_speed, stop_speed, current_elevator_floor)
+        # Create 4 initial calls
+        for _ in range(4):
+            yield self.env.timeout(1)
+            self.env.process(self.generate_user())
+            
+        # Create more button presses while the simulation is running
+        while True:
+            yield self.env.timeout(random.randint(2,12))
+            self.env.process(self.generate_user())
 
-    # # Create test cases
-    # env.process(user(env, 'Button Press 1', building_elevator, 12))
-    # yield env.timeout(20)
-    # env.process(user(env, 'Button Press 2', building_elevator, 6))
-    
-    # Create 4 initial calls
-    for i in range(4):
-        env.process(user(env, f'Button Press {i+1}', building_elevator, random.randint(2,19)))
-        yield env.timeout(1)
+    def run(self):
+        print("Starting Model")
+        random.seed(ModelConstants.random_seed)  
+        self.env.process(self.setup())
+        self.env.run(until=ModelConstants.sim_duration)
+        # print(f"Elevator is on Floor {current_floor}")
+        print(f"Total Completed Trips:  {total_trips}")
 
+# Run model
+my_model = ElevatorBank()
+my_model.run()
 
-    # Create more button presses while the simulation is running
-    while True:
-        yield env.timeout(random.randint(2,12))
-        i += 1
-        env.process(user(env, f'Button Press {i}', building_elevator, random.randint(1,19)))
-
-
-# Setup and start the simulation
-print('Elevator')
-
-random.seed(RANDOM_SEED)  # This helps reproducing the results
-
-# Create an environment and start the setup process
-env = simpy.Environment()
-env.process(setup(env, NUMBER_ELEVATORS, NORMAL_SPEED_ELEVATOR, STOP_SPEED_ELEVATOR, current_floor))
-
-# Execute!
-env.run(until=SIM_TIME)
-print(f"Elevator is on Floor {current_floor}")
-print(f"Total Completed Trips:  {total_trips}")
+print('Trips Completed: ', my_model.elevator_trips)
+print("Elevator Locations: ", my_model.elevator_floor)
+print("Total Distance (floors): ", my_model.total_floors)
---
+++ tests/elevator_initial_test.py
@@ -0,0 +1,109 @@
+import random
+import simpy
+
+
+RANDOM_SEED = 42
+NUMBER_ELEVATORS = 1
+NORMAL_SPEED_ELEVATOR = 3
+STOP_SPEED_ELEVATOR = 5
+
+# WASHTIME = 5      # Minutes it takes to clean a car
+REQUEST_INTERVAL = 7       # Create a car every ~7 minutes
+SIM_TIME = 120     # Simulation time in minutes
+
+# current_floor tracking doesn't work for multi-elevator system
+current_floor = 1
+total_trips = 0
+
+class ElevatorBank(object):
+
+    def __init__(self, env, num_elevators, normal_speed, stop_speed, current_elevator_floor):
+        self.env = env
+        self.elevator = simpy.Resource(env, num_elevators)
+        self.normal_speed = normal_speed
+        self.stop_speed = stop_speed
+        self.current_floor = current_elevator_floor
+
+    def call(self, name, floor):
+        global current_floor 
+        global total_trips
+
+        if current_floor > floor:
+            elevator_path = range(current_floor, floor, -1)
+            slow_floor = floor + 1
+            direction = -1
+        else:
+            elevator_path = range(current_floor, floor)
+            slow_floor = floor - 1
+            direction = 1
+
+        for elevator_floor in elevator_path:
+            if elevator_floor != slow_floor:
+                yield self.env.timeout(self.normal_speed)
+                print(f"{name}:  Located at Floor {elevator_floor+direction} at {env.now:.2f}")
+                current_floor = elevator_floor + direction
+            else:
+                yield self.env.timeout(self.stop_speed)
+                print(f"{name}:  Doors open on Floor {floor} at {env.now:.2f}")
+                current_floor = elevator_floor + direction
+                
+
+        yield self.env.timeout(2)
+        print(f'{name}: Doors close at {env.now:.2f}')
+
+        #  select new floor destination
+        #  initially make it go to floor one
+        first_floor = 1
+        for elevator_floor in range(current_floor-1, 0,-1):
+            if elevator_floor != first_floor:
+                yield self.env.timeout(self.normal_speed)
+                print(f"{name}:  Located at Floor {elevator_floor} at {env.now:.2f}")
+                current_floor = elevator_floor -1 
+            else:
+                yield self.env.timeout(self.stop_speed)
+                print(f"{name}:  Doors open on Floor 1 at {env.now:.2f}")
+                total_trips += 1
+
+def user(env, name, elevator_instance, call_floor):
+    print(f'{name} occurs at {env.now:.2f}.')
+    with elevator_instance.elevator.request() as request:
+        yield request   
+        print(f'{name}:  Elevator begins to move from Floor {current_floor} to Floor {call_floor} at {env.now:.2f}.')
+        yield env.process(elevator_instance.call(name, call_floor))
+
+
+def setup(env, num_elevators, normal_speed, stop_speed, current_elevator_floor):
+
+    building_elevator = ElevatorBank(env, num_elevators, normal_speed, stop_speed, current_elevator_floor)
+
+    # # Create test cases
+    # env.process(user(env, 'Button Press 1', building_elevator, 12))
+    # yield env.timeout(20)
+    # env.process(user(env, 'Button Press 2', building_elevator, 6))
+    
+    # Create 4 initial calls
+    for i in range(4):
+        env.process(user(env, f'Button Press {i+1}', building_elevator, random.randint(2,19)))
+        yield env.timeout(1)
+
+
+    # Create more button presses while the simulation is running
+    while True:
+        yield env.timeout(random.randint(2,12))
+        i += 1
+        env.process(user(env, f'Button Press {i}', building_elevator, random.randint(1,19)))
+
+
+# Setup and start the simulation
+print('Elevator')
+
+random.seed(RANDOM_SEED)  # This helps reproducing the results
+
+# Create an environment and start the setup process
+env = simpy.Environment()
+env.process(setup(env, NUMBER_ELEVATORS, NORMAL_SPEED_ELEVATOR, STOP_SPEED_ELEVATOR, current_floor))
+
+# Execute!
+env.run(until=SIM_TIME)
+print(f"Elevator is on Floor {current_floor}")
+print(f"Total Completed Trips:  {total_trips}")
